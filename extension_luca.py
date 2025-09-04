7fa1d079e6fcefe10423ef72fdc2634429081473
lucamuellercode <lucamueller.osk@googlemail.com>
2025-09-04 14:15:54 +0200

    I added further funcitons for introduction

--- extension_luca.py
+++ extension_luca.py
@@ -1,1 +1,11 @@
-print('Hi Friends')
+
+def introduce_yourself(name, age):
+    print(f'Hi m name is {name} and I am {age} years old')
+
+
+
+
+if __name__ == '__main__':
+    name = input("Enter your name: ")
+    age = input("Enter your age: ")
+    introduce_yourself()
